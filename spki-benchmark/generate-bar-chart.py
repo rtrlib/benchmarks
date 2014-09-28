6d7ee29bd15725d7ff9681357655504f4376ff6f
Nils Bars <nils.bars@haw-hamburg.de>
2014-09-28 15:55:18 +0200

    Add bar chart generate script

---
+++ spki-benchmark/generate-bar-chart.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+# Written by Nils Bars
+# Programming language: python3
+# using matplotlib 1.4.0-2
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+def print_usage():
+    print("Usage")
+    print("%s <chart-title> <source-file> <output-file>\n" % (sys.argv[0]))
+    print("The output data format will be determined by the file extension.")
+    print("Supported formats: eps, pdf, pgf, png, ps, raw, rgba, svg, svgz.\n")
+
+    print("The data in the source files must be in the following format:")
+
+    print("Example")
+    print("%s my-chart measurement1.log result.pdf\n" % (sys.argv[0]))
+    exit()
+
+# Parse arguments
+if len(sys.argv) != 4 or sys.argv[1] == '--help' or sys.argv[1] == '-h':
+    print_usage()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_title(sys.argv[1])
+
+# Get axis captions
+infile = open(sys.argv[2], 'r')
+captions = infile.readline()
+captionsList = captions.split(";")
+plt.xlabel(captionsList[0])
+plt.ylabel(captionsList[1])
+
+# Group names
+groupNames = infile.readline().strip().split(";")
+groupCount = len(groupNames)
+
+# Bar names
+barNames = infile.readline().strip().split(";")
+barCount = len(barNames)
+
+# Group data
+groupData = []
+
+line = infile.readline()
+while line != '':
+    barValues = line.strip().split(";")
+    barValues = [float(x) for x in barValues]
+    groupData.append(barValues)
+    line = infile.readline()
+
+ind = np.arange(groupCount)      # the x locations for the groups
+width = 0.05                    # the width of the bars
+
+# Draw the bars
+rects = []
+colors = ["b", "g", "r", "c", "m", "y", "k", "w"]
+for i in range(0, len(groupData)):
+    rect = ax.bar(
+        ind + (width * i), groupData[i], width, color=colors[i % len(colors)])
+    rects.append(rect)
+
+# Draw the x-axis group captions
+ax.set_xticks(ind + (width * barCount / 2))
+xtickNames = ax.set_xticklabels(groupNames)
+
+# add a legend
+ax.legend(rects, barNames, loc=2, fontsize="x-small")
+
+ax.grid()
+plt.savefig(sys.argv[len(sys.argv) - 1], dpi=1000)
+# plt.show()
