82b7260cb4ba51fe26ba4d35a70d247583e2bbd9
Nils Bars <nils.bars@haw-hamburg.de>
2014-09-28 16:31:45 +0200

    Add parameter to add_record benchmark

--- spki-benchmark/load_add_records_benchmark_draw_chart.sh
+++ spki-benchmark/load_add_records_benchmark_draw_chart.sh
@@ -4,29 +4,35 @@
 log_tmp_dir=tmp
 benchmark_result_dir=$(pwd)/result
 
-#Change this values
-start_value=1000 #Number of records added during first measurement.
-stop_value=100000 #Number of records added during last measurement.
-increment_value=5000 #Every measurement "number of records" will be increased by this value.
+if [[ "$#" != "3" ]]; then
+    echo "Usage: $0 start stop step"
+    exit
+fi
 
-rm -Rf $log_tmp_dir
-rm -Rf $benchmark_result_dir
+start_value=$1 #Number of records added during first measurement.
+stop_value=$2 #Number of records added during last measurement.
+increment_value=$3 #Every measurement "number of records" will be increased by this value.
 
-mkdir $log_tmp_dir
-mkdir $benchmark_result_dir
+rm -rf $log_tmp_dir
 
-#Add captions
+mkdir -p $log_tmp_dir
+mkdir -p $benchmark_result_dir
+
+#Add captions [x-axis/y-axis]
 echo "Number of added records;CPU time in s" > $log_tmp_dir/log
+echo "Data" >> $log_tmp_dir/log
 
 #Run benchmark and save results
 for (( i = $start_value; i <= $stop_value; i=$[$i+$increment_value] )); do
 	#Start benchmark -> add $i records and repeat the measurment 5 times
-	cpu_time_in_s=$(./src/load_add_entry_benchmark $i 5 | grep "Average" | cut -d ' ' -f 2)
+	cpu_time_in_s=$(./src/load_add_entry_benchmark $i 10 | grep "Average" | cut -d ' ' -f 2)
+    echo $cpu_time_in_s
 	echo "$i;$cpu_time_in_s" >> $log_tmp_dir/log
 done
 
 #Generate graph
-./generate-line-chart.py "Data" $log_tmp_dir/log $benchmark_result_dir/Add\ entry\ benchmark\ -\ $stop_value\ records.pdf
+./generate-line-chart.py "CPU time consumption of spki_table_add_entry()" $log_tmp_dir/log $benchmark_result_dir/CPU_time_add_entry.pdf
+echo ""
+echo "Add record benchmark result is in ${benchmark_result_dir}/"
 
-
-rm -R $log_tmp_dir
+rm -rf $log_tmp_dir
